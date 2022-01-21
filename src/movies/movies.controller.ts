fdaf25598ee65b1dccb7c50116503b353a6e07f7
shren207 <shren0812@gmail.com>
2022-01-21 18:58:34 +0900

    First commit

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(
+    @Query('year') searchingYear: string,
+    @Query('name') movieName: string,
+  ) {
+    return `We are searching for a movie with a title made after: ${searchingYear}, name is ${movieName}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return `This will return one movie with the id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
+  }
+}
